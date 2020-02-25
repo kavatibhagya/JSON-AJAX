83520f489f8af1e04da28a900e21b31e859f5318
kavatibhagya <bhagyalakshmi.kavati@gmail.com>
2020-02-24 20:22:21 -0800

    added some JSON js

--- javascript.js
+++ javascript.js
@@ -1,8 +1,18 @@
-var ourRequest = new XMLHttpRequest();
-ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json');
-ourRequest.onload = function(){
-    var ourData = ourRequest.responseText;
-    console.log(ourData[0]);
+var btn = document.getElementsById("btn");
+ btn.addEventListener('click', function(){
+    var ourRequest = new XMLHttpRequest();
+    ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json');
+    ourRequest.onload = function(){
+        var ourData = JSON.parse(ourRequest.responseText);
+        console.log(ourData[0]);
+    
+    };
+    ourRequest.send();
 
-};
-ourRequest.send();
+ })
+
+
+
+
+
+
