6bbec0a342046f00c76d487431c2a08579937df9
kavatibhagya <bhagyalakshmi.kavati@gmail.com>
2020-02-25 20:04:40 -0800

    added array of objects

--- javascript.js
+++ javascript.js
@@ -1,15 +1,37 @@
-var btn = document.getElementsById("btn");
+var pagecounter = 1;
+
+var animalContainer = document.getElementById('animal-info');
+var btn = document.getElementById("btn");
  btn.addEventListener('click', function(){
     var ourRequest = new XMLHttpRequest();
-    ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json');
+    ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-'+ pagecounter + '.json');
     ourRequest.onload = function(){
         var ourData = JSON.parse(ourRequest.responseText);
-        console.log(ourData[0]);
+        renderHTML(ourData);
+
     
     };
     ourRequest.send();
+    pagecounter++;
+    if(pagecounter > 3){
+        btn.classList.add("hide-me");
+    }
+ });
 
- })
+  function renderHTML(data){
+      var HTMLString = "";
+         
+      for(i=0; i<data.length; i++){
+          HTMLString += "<p>" + data[i].name + " is a " + data[i].species + "</p>";
+      }
+
+
+
+
+      animalContainer.insertAdjacentHTML('beforeend', HTMLString);
+      
+
+  }
 
 
 
